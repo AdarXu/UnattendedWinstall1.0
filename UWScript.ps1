3f23e84d0ad0e5617a1bfce21fd6db42b9ffa007
AdarXu <141630573+AdarXu@users.noreply.github.com>
2024-11-20 06:30:58 +0800

    Update UWScript.ps1

--- UWScript.ps1
+++ UWScript.ps1
@@ -508,14 +508,21 @@ function Install-AppWithWinGet {
 # Remove Bloatware Apps Functions
 # Define Packages
 $appxPackages = @(
-    'Microsoft.BingSearch', 'Clipchamp.Clipchamp', 'Microsoft.WindowsFeedbackHub',
-    'Microsoft.GetHelp', 'Microsoft.ZuneVideo', 'Microsoft.BingNews',
-    'Microsoft.MicrosoftOfficeHub', 'Microsoft.Office.OneNote', 'Microsoft.OutlookForWindows',
-    'Microsoft.People', 'MicrosoftCorporationII.QuickAssist', 'Microsoft.SkypeApp',
+    'Microsoft.Microsoft3DViewer', 'Microsoft.BingSearch', 'Microsoft.WindowsCamera', 'Clipchamp.Clipchamp',
+    'Microsoft.WindowsAlarms', 'Microsoft.549981C3F5F10', 'Microsoft.Windows.DevHome',
+    'MicrosoftCorporationII.MicrosoftFamily', 'Microsoft.WindowsFeedbackHub', 'Microsoft.GetHelp',
+    'microsoft.windowscommunicationsapps', 'Microsoft.WindowsMaps', 'Microsoft.ZuneVideo',
+    'Microsoft.BingNews', 'Microsoft.MicrosoftOfficeHub', 'Microsoft.Office.OneNote',
+    'Microsoft.OutlookForWindows', 'Microsoft.People', 'Microsoft.Windows.Photos',
+    'Microsoft.PowerAutomateDesktop', 'MicrosoftCorporationII.QuickAssist', 'Microsoft.SkypeApp',
     'Microsoft.MicrosoftSolitaireCollection', 'Microsoft.MicrosoftStickyNotes', 'MSTeams',
-    'Microsoft.Getstarted', 'Microsoft.Todos', 'Microsoft.ZuneMusic',
-    'Microsoft.OneDrive', 'Microsoft.MixedReality.Portal', 'Microsoft.Windows.Ai.Copilot.Provider',
-    'Microsoft.Copilot', 'Microsoft.Copilot_8wekyb3d8bbwe', 'Microsoft.WindowsMeetNow', 
+    'Microsoft.Getstarted', 'Microsoft.Todos', 'Microsoft.WindowsSoundRecorder', 'Microsoft.BingWeather',
+    'Microsoft.ZuneMusic', 'Microsoft.WindowsTerminal', 'Microsoft.Xbox.TCUI', 'Microsoft.XboxApp',
+    'Microsoft.XboxGameOverlay', 'Microsoft.XboxGamingOverlay', 'Microsoft.XboxIdentityProvider',
+    'Microsoft.XboxSpeechToTextOverlay', 'Microsoft.GamingApp', 'Microsoft.YourPhone', 'Microsoft.OneDrive',
+    'Microsoft.549981C3F5F10', 'Microsoft.MixedReality.Portal', 'Microsoft.ScreenSketch'
+    'Microsoft.Windows.Ai.Copilot.Provider', 'Microsoft.Copilot', 'Microsoft.Copilot_8wekyb3d8bbwe',
+    'Microsoft.WindowsMeetNow', 'Microsoft.WindowsStore', 'Microsoft.Paint', 'Microsoft.MSPaint'
 )
 
 # Define Windows Capabilities
